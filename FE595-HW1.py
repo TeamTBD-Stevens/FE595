202c78f67a726619777d753a198ab7f4bc06d031
TeamTBD-Stevens <62127144+TeamTBD-Stevens@users.noreply.github.com>
2020-10-02 12:59:27 -0400

    Update FE595-HW1.py

--- FE595-HW1.py
+++ FE595-HW1.py
@@ -1,16 +1,19 @@
-import matplotlib.pyplot as plt
-import numpy as np
+def project1():
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-x = np.arange(0, 2*np.pi, 0.1)
-y = np.sin(x)
-z = np.cos(x)
-tan = np.tan(x)
-
-plt.plot(x, y, x, z, x, tan)
-plt.ylim(-1, 1)
-plt.legend(labels=["sin", "cos", "tan"], loc="lower left")
+    x = np.arange(0, 2*np.pi, 0.1)
+    y = np.sin(x)
+    z = np.cos(x)
+    tan = np.tan(x)
+    
+    plt.plot(x, y, x, z, x, tan)
+    plt.ylim(-1, 1)
+    plt.legend(labels=["sin", "cos", "tan"], loc="lower left")
+    plt.show()
 
 if __name__ == "__main__":
-    plt.show()
+    project1()
+    
 
 
